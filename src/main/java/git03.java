7f7ab02172f93e6badf8d28719ee052ebb35aa5f
VictoriaCrismari <crismari.victoria1994@gmail.com>
2023-12-05 19:15:54 -0600

    V2

--- src/main/java/git03.java
+++ src/main/java/git03.java
@@ -1,5 +1,8 @@
 public class git03 {
     public static void main(String[] args) {
         System.out.println("Hello everyone!");
+        System.out.println("Hello everyone!");
+        System.out.println("Hello everyone!");
+        
     }
 }
